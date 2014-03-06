f4ae887f46e483fccc4cf272b50976ec726bc3c6
Lut Garcia <lutgardo21@gmail.com>
2014-03-06 10:12:17 -0600

    Fixed migration file Devise Create User, remove rU

--- db/migrate/20140126161319_devise_create_users.rb
+++ db/migrate/20140126161319_devise_create_users.rb
@@ -5,7 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :first_name
       t.string :last_name
       t.string :profile_name
-      rU
+      
       ## Database authenticatable
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
