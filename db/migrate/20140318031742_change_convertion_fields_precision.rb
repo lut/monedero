6aaf6907802c0cf6182dcf9c41e49d87eb036d2a
Lut Garcia <lutgardo21@gmail.com>
2014-03-17 21:23:17 -0600

    Updated DB Merchant & Credit Conversion Rate Precision

--- db/migrate/20140318031742_change_convertion_fields_precision.rb
+++ db/migrate/20140318031742_change_convertion_fields_precision.rb
@@ -1,7 +1,7 @@
 class ChangeConvertionFieldsPrecision < ActiveRecord::Migration
   def change
-  	change_column :credits, :convertion_rate, :precision => 1
-  	change_column :merchants, :convertion_rate, :precision => 1
+  	change_column :credits, :convertion_rate, :decimal, precision: 1, scale: 2
+  	change_column :merchants, :convertion_rate, :decimal, precision: 1, scale: 2
   end
 
 end
